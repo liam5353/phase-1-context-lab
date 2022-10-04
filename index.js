a8ed1cc2042f822cfb9d4b6fcd27f2080cd610e1
“Liam <okeefe.liam@yahoo.com>
2022-10-04 11:55:25 -0500

    commit tree 7150855ddb7084d51b805d0d2bb3ecabc6f47283

--- index.js
+++ index.js
@@ -1,4 +1,67 @@
 /* Your Code Here */
+function createEmployeeRecord(employee) {
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees) {
+    return employees.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(object, dateStamp) {
+    let [date, hour] = dateStamp.split(" ")
+    let eventObj = {
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date
+    }
+
+    this.timeInEvents.push(eventObj)
+    return object 
+}
+
+function createTimeOutEvent(object, dateStamp) {
+    let [date, hour] = dateStamp.split(" ")
+    let eventObj = {
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date
+    }  
+
+    object.timeOutEvents.push(eventObj)
+    return object 
+}
+
+function hoursWorkedOnDate(object, dateStamp) {
+    const timeIn = object.timeInEvents.find(event => event.date === dateStamp)
+    const timeOut = object.timeOutEvents.find(event => event.date === dateStamp)
+    return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(object, date) {
+    const hours = hoursWorkedOnDate (object, date)
+    return object.payPerHour * hours
+}
+
+function calculatePayroll(employeeRecords) {
+    //  const record = employeeRecords.map(employee => allWagesFor.call(employee))
+
+    //  record.reduce((currentValue, total) => currentValue = total)
+    
+    //  return record.reduce((currentValue, total) => currentValue + total)
+
+    return employeeRecords.map(employee => allWagesFor(employee)).reduce((currentValue, total) => currentValue + total)
+}
+
+function findEmployeeByFirstName(employees, firstNameString) {
+    return employees.find(emp => emp.firstName === firstNameString)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
